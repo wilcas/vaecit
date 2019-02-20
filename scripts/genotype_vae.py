08cc7848eeaf119e95a8badf7989f235f9f29b49
William Casazza <willcasazza@gmail.com>
2019-02-20 12:02:37 -0800

    simplifiying a bit, doesn't really work, found easier example to work from, will rebuild from there

--- scripts/genotype_vae.py
+++ scripts/genotype_vae.py
@@ -3,10 +3,8 @@ An implementation of a Variational Autoencoder (VAE) that trains on a genotype m
 goal of the autoencoder is to learn a latent representation of genotypes from which it can
 reconstruct genotypes with similar nonlinear relationships with each other between samples.
 """
-import functools
+import numpy as np
 import tensorflow as tf
-import tensorflow_probability as tfp
-
 
 tfd = tfp.distributions
 
@@ -14,17 +12,15 @@ tfd = tfp.distributions
 class vae(object):
     
     
-    def __init_(self, n_hidden=1, n_latent=2):
+    def __init__(self, n_hidden=1, n_latent=50):
         self.n_hidden = n_hidden
         self.n_latent = n_latent
-        self.data = data
-        raise NotImplemented
         
     
     def build_encoder(self):
         net_layers = [tf.keras.layers.Flatten()]
         for i in range(self.n_hidden):
-            tmp_layer = tf.keras.layers.Dense(2 * latent_size, activation=None)
+            tmp_layer = tf.keras.layers.Dense(2 * self.n_latent, activation=None)
             net_layers.append(tmp_layer)
         encode_net = tf.keras.Sequential(net_layers)
         
@@ -33,17 +29,16 @@ class vae(object):
             NN = encode_net(X)
             return tfd.MultivariateNormalDiag(
                 loc=NN[..., :self.n_latent],
-                scale_diag=tf.nn.softplus(net[..., self.n_latent:] + _softplus_inverse(1.0)),
+                scale_diag=tf.nn.softplus(NN[..., self.n_latent:] + _softplus_inverse(1.0)),
                 name="encode"
             )        
         return encoder
     
     
-    def make_decoder(self, data):
-        output_shape = tf.shape(data)
+    def build_decoder(self, output_shape):
         net_layers = [tf.keras.layers.Flatten()]
         for i in range(self.n_hidden):
-            tmp_layer = tf.keras.layers.Dense(2 * latent_size, activation=None)
+            tmp_layer = tf.keras.layers.Dense(2 * self.n_latent, activation=None)
             net_layers.append(tmp_layer)
         decode_net = tf.keras.Sequential(net_layers)
         def decoder(codes):
@@ -51,53 +46,13 @@ class vae(object):
             codes = tf.reshape(codes, (-1, 1, 1, self.n_latent))
             logits = decode_net(codes)
             logits = tf.reshape(
-                norm_samples,
-                shape= tf.concat([orig_shape[:-1], output_shape])
+                logits,
+                shape= tf.concat([orig_shape[:-1], output_shape], axis=0)
             )
             return tfd.Independent(
-                tfd.Binomial(logits = logits, total_count=2),
+                tfd.Binomial(logits=logits, total_count=2),
                 reinterpreted_batch_ndims=len(output_shape),
                 name='genotypes'
             )
         return decoder
        
-
-    def encode(self):
-        raise NotImplemented
-    
-    def decode(self):
-        raise NotImplemented
-
-    def model_fn(self,data,mode):
-        encoder = self.make_encoder()
-        decoder = self.make_decoder(data)
-        approx_posterior = encoder(data)
-        approx_posterior_sample = approx_posterior.sample(100)
-        decoder_likelihood = decoder(approx_posterior_sample)
-        distortion = -decoder_likelihood.log_prob(data)
-        avg_distortion = tf.reduce_mean(input_tensor=distortion)
-        rate = tfd.kl_divergence(approx_posterior, tfd.MultivariateNormalDiag(loc=tf.zeros([self.n_latent]), scale_identity_multiplier=1.0))
-        avg_rate = tf.reduce_mean(input_tensor=rate)
-        elbo_local = -(rate + distortion)
-        elbo = tf.reduce_mean(input_tensor=elbo_local)
-        loss = -elbo
-        global_step = tf.compat.v1.train.get_or_create_global_step()
-        learning_rate = tf.compat.v1.train.cosine_decay(
-            0.001,
-            global_step,
-            1000
-        )
-        optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate)
-        train_op = optimizer.minimize(loss, global_step=global_step)
-        return tf.estimator.EstimatorSpec(
-            mode=mode,
-            train_op=train_op,
-            eval_metric_ops ={
-                "elbo": tf.compat.v1.metrics.mean(elbo),
-                "rate": tf.compat.v1.metrics.mean(avg_rate),
-                "distortion":  tf.compat.v1.metrix.mean(avg_distortion)
-            }
-        )
-        
-        
-        
