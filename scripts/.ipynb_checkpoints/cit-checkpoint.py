aa809e211094049c92aaf20208d3b689348763c9
William Casazza <willcasazza@gmail.com>
2019-02-28 18:39:28 -0800

    close #1 and close #2, still need to test some causal inference stuff with pca and VAEs

---
+++ scripts/.ipynb_checkpoints/cit-checkpoint.py
@@ -0,0 +1,94 @@
+import numpy as np
+from scipy import stats
+from numba import jit
+
+def ftest(fit1, fit2, n):
+    RSS1 = fit1['rss']
+    RSS2 = fit2['rss']
+    p1 = fit1['beta'].size
+    p2 = fit2['beta'].size
+    fstat = ((RSS1 - RSS2) / (p2-p1)) / (RSS2 / (n - p2))
+    return 1 - stats.f.cdf(fstat, p2-p1,n-p2), fstat
+    
+def linreg_with_stats(y, X=None):
+    if X is None:
+        X = np.ones(shape = (y.shape[0],1))
+    n = X.shape[0]
+    data = np.c_[np.ones(n), X]
+    beta, _, _, _ = np.linalg.lstsq(data,y, rcond=None)
+    y_bar = np.mean(y)
+    TSS = np.sum(np.square(y - y_bar))
+    RSS = np.sum(np.square(y - data@beta))
+    var_beta = RSS * np.linalg.pinv(data.T@data)
+    se = np.sqrt(np.diag(var_beta))
+    res_dict = {
+        'beta': beta,
+        'rss': RSS,
+        'se': se,
+        't': beta / se,
+        'p' :  (1 - stats.t.cdf(beta / se, df = n - 2)),
+        'r2': 1 - (RSS / TSS)
+    }
+    return res_dict
+
+def test1(T, L):
+    n = T.shape[0]
+    fit1 = linreg_with_stats(T, L)
+    fit2 = linreg_with_stats(T)
+    p, _ = ftest(fit2, fit1, n)
+    return fit1, p
+    
+def test2(T, G, L):
+    n = T.shape[0]
+    fit1 = linreg_with_stats(G, np.c_[T, L])
+    fit2 = linreg_with_stats(G, T)
+    p, _ = ftest(fit2, fit1, n)
+    return fit1, p
+    
+def test3(T, G, L):
+    n = T.shape[0]
+    fit1 = linreg_with_stats(T, np.c_[G, L])
+    fit2 = linreg_with_stats(T, G)
+    p, _ = ftest(fit2, fit1, n)
+    return fit1, p
+    
+    
+def test4(T, G, L, num_bootstrap):
+    n = T.shape[0]
+    fit = linreg_with_stats(G, L)
+    beta = fit['beta']
+    residual = G - np.c_[np.ones(n),L]@beta
+    f_list = [None for i in range(num_bootstrap)]
+    for i in range(num_bootstrap):
+        np.random.shuffle(residual)
+        fitA = linreg_with_stats(T,np.c_[residual, L])
+        fitB = linreg_with_stats(T, residual)
+        _, fstat = ftest(fitB, fitA, n)
+        f_list[i] = fstat
+    fit1 = linreg_with_stats(T, np.c_[G, L])
+    fit2 = linreg_with_stats(T, G)
+    _, fstat = ftest(fit2, fit1, n)
+    p = sum([fstat > f for f in f_list]) / num_bootstrap
+    return fit1, p
+
+def cit(target, mediator, instrument, num_bootstrap=10000):
+    # run tests
+    stats1, p1 = test1(target, instrument)
+    stats2, p2 = test2(target, mediator, instrument)
+    stats3, p3 = test3(target, mediator, instrument)
+    stats4, p4 = test4(target, mediator, instrument, num_bootstrap)
+    omni_p = max(p1, p2, p3, p4)
+    
+    # merge stats into one table
+    res = {
+        'test1': stats1,
+        'p1': p1,
+        'test2': stats2,
+        'p2': p2,
+        'test3': stats3,
+        'p3': p3,
+        'test4': stats4,
+        'p4': p4,
+        'omni_p': omni_p
+    }
+    return res
--- scripts/vae.py
+++ scripts/vae.py
@@ -16,55 +16,55 @@ def log_normal_pdf(sample, mean, logvar, raxis=1):
 
 
 class VAE(tf.keras.Model):
-    def __init__(self, n_latent=2, output_size=10): # default 2 latent dimensions
+    def __init__(self, output_size, n_latent=2, n_hidden=2): # default 2 latent dimensions
         super(VAE,self).__init__() # inherit Model functions
-        self.n_latent=n_latent
-        self.encode_net = tf.keras.Sequential(
-            [
-                tf.keras.layers.Dense(1276,activation=tf.nn.relu),
-                tf.keras.layers.Dense(127,activation=tf.nn.relu),
-                tf.keras.layers.Dense(2*n_latent) #no Activation
-            ]
-        )
-        self.decode_net = tf.keras.Sequential(
-            [
-                tf.keras.layers.InputLayer(input_shape=(n_latent,)),
-                tf.keras.layers.Dense(1276,activation=tf.nn.relu),
-                tf.keras.layers.Dense(127,activation=tf.nn.relu),
-                tf.keras.layers.Dense(output_size) #no Activation
-            ]
-        )
+        self.n_latent = n_latent
+        self.n_hidden = n_hidden
+        self.encode_net = tf.keras.Sequential()
+        for i in range(n_hidden):
+            self.encode_net.add(tf.keras.layers.Dense(128 * (i+1),activation=tf.nn.relu))
+        self.encode_net.add(tf.keras.layers.Dense(2*n_latent)) #no Activation
+        self.decode_net = tf.keras.Sequential()
+        self.decode_net.add(tf.keras.layers.InputLayer(input_shape=(n_latent,)))
+        for i in range(n_hidden)
+            self.decode_net.add(tf.keras.layers.Dense(128 * (n_hidden - i + 1),activation=tf.nn.relu))        
+        tf.keras.layers.Dense(output_size) #no Activation
+        
     
     def call(self, data):
         mean,logvar = self.encode(data)
         z = self.reparameterize(mean, logvar)
         return self.decode(z)
 
+    
     def sample(self, eps=None):
         if eps is None:
             eps = tf.random.normal(shape=(100, self.n_latent))
         return self.decode(eps, apply_sigmoid=True)
 
+    
     def encode(self, x):
         mean, logvar = tf.split(self.encode_net(x), num_or_size_splits=2, axis=1)
         return mean, logvar
 
+    
     def reparameterize(self, mean, logvar):
         eps = tf.random.normal(shape=mean.shape)
         return eps * tf.exp(logvar * .5) + mean
 
+    
     def decode(self, z, apply_sigmoid = False):
         logits = self.decode_net(z)
         if apply_sigmoid:
             return tf.sigmoid(logits)
         else:
             return logits
-        
+    
+    
     def total_loss(self, x,y):
         mean, logvar = self.encode(x)
         z = self.reparameterize(mean, logvar)
         x_logit = self.decode(z)
-
         cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x)
         logpx_z = -tf.reduce_sum(cross_ent)
         logpz = log_normal_pdf(z, 0., 0.)
