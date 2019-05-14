26effc431138b7d9a7bbd9c3ff4fd7b701bdbd02
William Casazza <willcasazza@gmail.com>
2019-05-14 11:53:49 -0700

    dimension in compute pcs

--- scripts/data_model.py
+++ scripts/data_model.py
@@ -192,15 +192,20 @@ def compute_pcs(A):
 
 
 def get_mediator(data, ids, which_ids, data2= None, ids2 = None, which_ids2 = None):
+    n = data.shape[0]
     feature_idx = np.isin(ids, which_ids)
     tmp_data = data[:,feature_idx]
     if not np.isscalar(data2) and data2 is not None:
         feature_idx2 = np.isin(ids2, which_ids2)
         tmp_data2 = data2[:, feature_idx2]
         cur_data = np.concatenate((tmp_data, tmp_data2), axis=1)
-        cur_data = compute_pcs(cur_data)[:, 0]
+        cur_data = compute_pcs(cur_data)
     else:
-        cur_data = compute_pcs(tmp_data)[:, 0]
+        cur_data = compute_pcs(tmp_data)
+    if len(cur_data.shape) == 1:
+        cur_data = cur_data.reshape((n,1))
+    else:
+        cur_data = cur_data[:,0]
     return cur_data
 
 
