841247d9add1a5d6229daa18ed8254230dc6167e
willcasazza@gmail.com <willcasazza@gmail.com>
2020-06-17 13:53:19 -0700

    steiger MR code

--- scripts/mendelian_randomization.py
+++ scripts/mendelian_randomization.py
@@ -14,6 +14,43 @@ doesn't fit a complicated LMM so will likely be implemented here if used.
    -Steiger filtering
 
 """
+import numpy as np
+import pandas as pd
+from statsmodels.sandbox.regression.gmm import IV2SLS 
 
-import statsmodels.api as sm
-import data_model as dm
+
+def write_csv(results, filename):
+    with open(filename, 'w') as f:
+        names = results[0].keys()
+        writer = csv.DictWriter(f, names)
+        writer.writeheader()
+        writer.writerows(results)
+
+
+def MR(exposure, outcome, SNP):
+    model = IV2SLS(outcome,exposure,SNP)
+    res = model.fit()
+    return {
+      'pMR': res.pvalues[-1],
+      'bMR': res.params[-1]
+    }
+
+
+def steiger(exposure, outcome, SNP):
+    rho_gy = np.corcoef(SNP,outcome)
+    rho_gx = np.corcoef(SNP,exposure)
+    rho_xy = np.corcoef(exposure,outcome)
+    Z_gy = 0.5 * np.log((1.0 + rho_gy) / (1.0 - rho_gy))
+    Z_gx = 0.5 * np.log((1.0 + rho_gx) / (1.0 - rho_gx))
+    rm_2 = 0.5 * (rho_gx**2 + rho_gy**2)
+    f = (1.0 - rho_xy) / (2.0 *(1 - rm_2))
+    h = (1.0 - f*rm_2) /(1.0 - rm_2)
+    Z = (Z_gx - Z_gy) * (np.sqrt(np.size(exposure)[1] - 3.0) / np.sqrt(2.0*(1 - rho_xy)*h))
+    return Z
+
+
+def steiger_MR(exposure,outcome,SNP):
+    mr = MR(exposure,outcome, SNP)
+    Z = steiger(exposure, outcome, SNP)
+    mr['Z'] = Z
+    return mr
